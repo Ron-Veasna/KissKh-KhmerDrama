c64b8d8c19470ccf2986b0f6db16b898bbd3b678
Ron Veasna <vaing133@gmail.com>
2023-11-22 20:12:15 +0700

    New Update

---
+++ Sna-KissKh/web2.js
@@ -0,0 +1,32 @@
+document.getElementById("Install").addEventListener("click", function(){
+    alert("Sorry, you can't install the application")
+    console.log("No yet application, need app developer to create a new");
+})
+document.getElementById("faq").addEventListener("click", function(){
+    alert("Sorry, No FAQ on this website yet")
+    console.log("please do FAQ on this website for more customers");
+})
+document.getElementById("RQ").addEventListener("click", function(){
+    alert("Sorry, No Drama or movie to request on this website yet")
+    console.log("please do request drama and movie on this website for more customers to check thier favorites");
+})
+document.getElementById("theme").addEventListener("click", function(){
+    alert("Sorry, No theme on this website yet but, we will do day mode and night mode on this website");
+    console.log("please do theme on this website to fit the customers favorites");
+})
+document.getElementById("epl").addEventListener("click", function(){
+    alert("Sorry, No explore on this website yet but, we will do explore on this website soon");
+    console.log("please do explore on this website for our customers upload");
+})
+document.getElementById("search").addEventListener("click", function(){
+    alert("Sorry, No search on this website yet but, we will do search on this website soon for you to search your favorites drama and movies");
+    console.log("please do search on this website for our customers search thier favorites drama and movies");
+})
+document.getElementById("login").addEventListener("click", function(){
+    alert("Sorry, No login on this website yet but, we will do login on this website soon");
+    console.log("please do login on this website for our customers to login");
+})
+document.getElementsByClassName("box-d").addEventListener("click", function(){
+    alert("Sorry, No drama or movies on this website yet but, we will do drama on this website soon");
+    console.log("please do drama or movies on this website for our customers to watch");
+})
